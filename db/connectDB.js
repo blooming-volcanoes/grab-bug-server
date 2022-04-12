4f0618dd0127a75e72716b7b1418e4e25a8dd401
Raiyan <raiyan5764@gmail.com>
2022-04-12 12:34:35 +0600

    [EDITED]- connectDb edited

--- db/connectDB.js
+++ db/connectDB.js
@@ -2,7 +2,7 @@
 /* eslint-disable camelcase */
 const mongoose = require('mongoose');
 
-const db_uri = process.NODE_ENV === 'production' ? process.env.DB_ATLAS_URI : process.env.DB_LOCAL_URI;
+const db_uri = process.env.NODE_ENV === 'production' ? process.env.DB_ATLAS_URI : process.env.DB_LOCAL_URI;
 
 const connectDb = async () => {
     try {
