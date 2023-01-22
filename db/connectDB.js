3d844135059cd9cc710d38da50e9aa338af5f8d1
Awal hossain <waqin21@gmail.com>
2023-01-22 12:00:59 +0600

    db uri changed ✌️

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,9 +1,9 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable camelcase */
 const mongoose = require('mongoose');
+// process.env.NODE_ENV === 'production' ? process.env.DB_ATLAS_URI : process.env.DB_LOCAL_URI;
 
-const db_uri =
-    process.env.NODE_ENV === 'production' ? process.env.DB_ATLAS_URI : process.env.DB_LOCAL_URI;
+const db_uri = process.env.DB_ATLAS_URI;
 
 const connectDb = async () => {
     try {
